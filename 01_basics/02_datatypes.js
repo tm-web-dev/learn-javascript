5365cb8eabe5049326151f7aaa78e05e0f0b8bd8
TM Web Dev <149615395+tm-web-dev@users.noreply.github.com>
2024-11-15 09:49:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // We are using node js not browser
+
+let name = "Thakur"
+let age = 25
+let isLoggedIn = false
+let state = null
+
+//number 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not defined
+//symbol => unique
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
