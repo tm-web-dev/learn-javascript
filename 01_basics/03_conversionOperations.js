999e994a35f72f6c4469a13c5020395884781b1a
TM Web Dev <149615395+tm-web-dev@users.noreply.github.com>
2024-11-15 10:02:45 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1, false => 0
+
+//Boolean conversion 
+// true =>1, false => 0
+// "" => false
+// "abc" => true
+
+//String conversion
+
+// 33 => "33" can converted to string
