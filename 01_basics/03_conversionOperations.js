42577c8d73a4485da175ef33a6ffeb3925597205
TM Web Dev <149615395+tm-web-dev@users.noreply.github.com>
2024-11-15 10:21:53 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,13 +1,13 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -22,3 +22,38 @@ console.log(valueInNumber);
 //String conversion
 
 // 33 => "33" can converted to string
+
+
+
+// *************************************Operations*******************************************
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+
+let str1 = "Hello"
+let str2 = "Thakur"
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let val = 1
+ ++val //Prefix increment => the increment operator increments and returns the value after incrementing.
+ val++ //Postfix Increment => the increment operator increments and returns the value before incrementing.
+
+ //link to study
+ //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
